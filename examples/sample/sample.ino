6698b41bfd87c0d7c6a3b07d664996cb296bd4e8
Goji <goji2100@gmail.com>
2022-05-08 20:12:29 +0900

    for ESP32 Devkit

--- examples/sample/sample.ino
+++ examples/sample/sample.ino
@@ -3,8 +3,8 @@
 ESP32softPWM PWM;
 #define MAXVAL ESP32softPWM_MAXVAL
 
-//uint8_t PWMpins[] = { 2, 4, 5, 16, 17, 18, 19, 21 };
-uint8_t PWMpins[] = { 26, 25, 17, 16, 27, 14, 12, 13,  5, 23, 19, 18, 22, 21 };
+uint8_t PWMpins[] = { 2, 4, 5, 16, 17, 18, 19, 21 };
+//uint8_t PWMpins[] = { 26, 25, 17, 16, 27, 14, 12, 13,  5, 23, 19, 18, 22, 21 };
 
 void setup() {
   Serial.begin(115200);
